9426df81e10d5a520b81a9d658fa8fb82eb2c370
Wins Tabar <wtabar@Winsumac.local>
2016-11-02 00:39:00 +0800

    Try
    Commit again branch

--- HelloGitHub/ViewController.swift
+++ HelloGitHub/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
         print()
         // So this is in 'try-branch' now? 
+        // Try if branch appears again.
     }
 
 
